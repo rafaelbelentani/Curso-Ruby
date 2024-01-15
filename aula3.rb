4f79fb79f03bfa818b249586d54bebf93873e8c1
Rafael Luiz Belentani <rafaelluiz_belentani@hotmail.com>
2024-01-15 18:59:44 -0300

    up

---
+++ aula3.rb
@@ -0,0 +1,10 @@
+print "Olá, qual é o seu primeiro nome?"
+n1 = gets.chomp
+
+print "Qual o seu sobrenome?"
+n2 = gets.chomp
+
+print "Qual sua idade?"
+idade = gets.chomp
+
+print "Olá #{n1} #{n2} você tem #{idade} anos"
---
+++ equacoes.rb
@@ -0,0 +1,9 @@
+puts "Digite um Numero"
+n1 = gets.chomp.to_i
+
+puts "Digite o segundo Numero"
+n2 = gets.chomp.to_i
+
+resposta = n1 * n2
+
+puts "#{resposta}"
